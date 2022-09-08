771f26f63f169ba085336880d0b538143d25a43d
webdesigntuts <hostelidis99@gmail.com>
2022-09-09 01:53:55 +0300

    nav

--- client/src/components/Navbar/MobileNavbar.jsx
+++ client/src/components/Navbar/MobileNavbar.jsx
@@ -22,107 +22,119 @@ const MobileNavbar = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <div>
-        <div
-          className={`${styles.iconContainer} ${styles.bars}`}
-          onClick={() => setNavOpen(true)}
-        >
-          <FaBars />
+    <>
+      <div className={styles.container}>
+        <div>
+          {/* CLOSE BUTTON */}
+          <div
+            className={`${styles.iconContainer} ${styles.bars}`}
+            onClick={() => setNavOpen(true)}
+          >
+            <FaBars />
+          </div>
+
+          {/* LINKS */}
+          <nav className={navOpen ? styles.navActive : undefined}>
+            <ul>
+              <div
+                className={`${styles.iconContainer} ${styles.times}`}
+                onClick={() => setNavOpen(false)}
+              >
+                <FaTimes />
+              </div>
+
+              {/* HOME */}
+              <ListItemLink
+                url=''
+                optionClass={styles.linkColor}
+                clickHandler={closeNav}
+              >
+                <h3>Home</h3>
+              </ListItemLink>
+
+              {/* CATEGORIES */}
+              <ListItemLink
+                url='categories'
+                optionClass={styles.linkColor}
+                clickHandler={closeNav}
+              >
+                <h3>Categories</h3>
+              </ListItemLink>
+
+              {/* TRANSACTIONS */}
+              <ListItemLink
+                url='transactions'
+                optionClass={styles.linkColor}
+                clickHandler={closeNav}
+              >
+                <h3>Transactions</h3>
+              </ListItemLink>
+
+              {/* Wallet */}
+              <ListItemLink
+                url='wallet'
+                optionClass={styles.linkColor}
+                clickHandler={closeNav}
+              >
+                <h3>Wallet</h3>
+              </ListItemLink>
+
+              {/* Profile */}
+              <div className={styles.mobileMenuLinks}>
+                <ListItemLink
+                  url='profile'
+                  optionClass={styles.linkColor}
+                  clickHandler={closeNav}
+                >
+                  <h3>Profile</h3>
+                </ListItemLink>
+              </div>
+
+              {/* Settings */}
+              <div className={styles.mobileMenuLinks}>
+                <ListItemLink
+                  url='settings'
+                  optionClass={styles.linkColor}
+                  clickHandler={closeNav}
+                >
+                  <h3>Settings</h3>
+                </ListItemLink>
+              </div>
+
+              {/* LOGOUT*/}
+              <button
+                url='logout'
+                onClick={() => {
+                  logoutHandler(null, {
+                    onSuccess: () => {
+                      queryClient.removeQueries();
+                      queryClient.cancelQueries();
+
+                      navigate("/auth");
+                    },
+                  });
+                }}
+                style={{
+                  background: "transparent",
+                  cursor: "pointer",
+                  color: "whitesmoke",
+                  fontWeight: "500",
+                }}
+              >
+                <h3>Logout</h3>
+              </button>
+            </ul>
+          </nav>
         </div>
-        <nav className={navOpen ? styles.navActive : undefined}>
-          <ul>
-            <div
-              className={`${styles.iconContainer} ${styles.times}`}
-              onClick={() => setNavOpen(false)}
-            >
-              <FaTimes />
-            </div>
-
-            {/* HOME */}
-            <ListItemLink
-              url=''
-              optionClass={styles.linkColor}
-              clickHandler={closeNav}
-            >
-              <h3>Home</h3>
-            </ListItemLink>
-
-            {/* CATEGORIES */}
-            <ListItemLink
-              url='categories'
-              optionClass={styles.linkColor}
-              clickHandler={closeNav}
-            >
-              <h3>Categories</h3>
-            </ListItemLink>
-
-            {/* TRANSACTIONS */}
-            <ListItemLink
-              url='transactions'
-              optionClass={styles.linkColor}
-              clickHandler={closeNav}
-            >
-              <h3>Transactions</h3>
-            </ListItemLink>
-
-            {/* Wallet */}
-            <ListItemLink
-              url='wallet'
-              optionClass={styles.linkColor}
-              clickHandler={closeNav}
-            >
-              <h3>Wallet</h3>
-            </ListItemLink>
-
-            {/* Profile */}
-            <div className={styles.mobileMenuLinks}>
-              <ListItemLink
-                url='profile'
-                optionClass={styles.linkColor}
-                clickHandler={closeNav}
-              >
-                <h3>Profile</h3>
-              </ListItemLink>
-            </div>
-
-            {/* Settings */}
-            <div className={styles.mobileMenuLinks}>
-              <ListItemLink
-                url='settings'
-                optionClass={styles.linkColor}
-                clickHandler={closeNav}
-              >
-                <h3>Settings</h3>
-              </ListItemLink>
-            </div>
-
-            {/* AUTH MENU */}
-            <button
-              url='logout'
-              onClick={() => {
-                logoutHandler(null, {
-                  onSuccess: () => {
-                    queryClient.removeQueries();
-                    queryClient.cancelQueries();
-
-                    navigate("/auth");
-                  },
-                });
-              }}
-              style={{
-                background: "transparent",
-                cursor: "pointer",
-                color: "whitesmoke",
-                fontWeight: "500",
-              }}
-            >
-              <h3>Logout</h3>
-            </button>
-          </ul>
-        </nav>
       </div>
-    </div>
+
+      {/* BLACK BACKGROUND */}
+      <div
+        onClick={() => setNavOpen(false)}
+        className={styles.blackBg}
+        style={navOpen ? { transform: "translateX(0)" } : undefined}
+      />
+    </>
   );
 };
 
