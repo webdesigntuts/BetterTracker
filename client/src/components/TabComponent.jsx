aa6785fb077d110497665cd84ee1a41cdffec0aa
webdesigntuts <hostelidis99@gmail.com>
2022-09-08 15:52:54 +0300

    stuff

--- client/src/components/TabComponent.jsx
+++ client/src/components/TabComponent.jsx
@@ -1,12 +1,20 @@
 import styles from "../styles/TabComponent.module.scss";
 import { Link, Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const TabComponent = ({ Tabs }) => {
+  const { pathname } = useLocation();
   return (
     <div className={styles.container}>
       <div className={styles.tabs}>
         {Tabs?.map((tab, index) => (
-          <Link key={index} to={`/categories/${tab?.link}`}>
+          <Link
+            key={index}
+            to={`/categories/${tab?.link}`}
+            className={
+              `/categories/${tab?.link}` === pathname ? styles.active : ""
+            }
+          >
             {tab?.name}
           </Link>
         ))}
